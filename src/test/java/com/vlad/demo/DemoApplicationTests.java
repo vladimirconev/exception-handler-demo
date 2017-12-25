8c3b50a65662ed0a88e44e94937ee3e77243c967
Vladimir Conev <vladimir.conev@seavus.com>
2017-12-25 10:30:59 +0100

    add io.rest-assured and create simple test that verifies 404 is thrown on get Task

--- src/test/java/com/vlad/demo/DemoApplicationTests.java
+++ src/test/java/com/vlad/demo/DemoApplicationTests.java
@@ -2,15 +2,27 @@ package com.vlad.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.Is.is;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class DemoApplicationTests {
 
+	@LocalServerPort
+	protected int port;
+
 	@Test
-	public void contextLoads() {
+	public void getTaskTest(){
+		given().when().
+				port(this.port).
+				get("/v1/task/1243").
+				then().assertThat().
+				statusCode(is(404));
 	}
 
 }
